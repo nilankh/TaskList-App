2845b4f40b627a46c8f59f9de2b3dd2588b63c3d
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-13 16:16:01 +0530

    filter by search implement successfully

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ function loadEventListeners() {
     taskList.addEventListener('click', removeTask);
     // clear task event
     clearBtn.addEventListener('click', clearTasks);
+    // Filter tasks event
+    filter.addEventListener('keyup', filterTasks);
+
+
 }
 
 // Add Task
@@ -73,4 +77,18 @@ function clearTasks(){
     }
     // https://jsperf.com/innerhtml-vs-removechild
 
+}
+
+// Filter Tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
+    // console.log(text);
 }
