345ffd453ae9b04f08bfa070092a67643b46af72
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-13 15:16:48 +0530

    addded tasks to display

---
+++ app.js
@@ -0,0 +1,47 @@
+// define Ui variable
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+//Load all event Listeners
+loadEventListeners();
+
+// Load all even listeners
+function loadEventListeners() {
+    // add task event
+    form.addEventListener('submit', addTask);
+
+}
+
+// Add Task
+function addTask(e){
+    if(taskInput.value == ''){
+        alert('Add a task');
+    }
+    
+    // Create li element
+    const li = document.createElement('li');
+    // Addd Class
+    li.className = 'collection-item';
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // create new link element
+    const link = document.createElement('a');
+    // Add Class
+    link.className = 'delete-item secondary-content';
+    // Add icon html
+    link.innerHTML = '<i class = "fa fa-remove"></i>';
+    // append the link to li
+    li.appendChild(link);
+
+    // append li to ul
+    taskList.appendChild(li);
+
+    // console.log(li);
+    // Clear input
+    taskInput.value = '';
+    
+    e.preventDefault();
+}
